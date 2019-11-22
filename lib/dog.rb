42dcfceaaee19adb85f61efed5a1023f81515b1e
Jeremy Bourne <jbourne25@yahoo.com>
2019-11-22 21:03:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,5 @@
 class Dog
+  @@all = []
   attr_accessor :owner, :mood 
   attr_reader :name 
   
@@ -6,9 +7,13 @@ class Dog
     @name = name 
     @owner = owner
     @mood = "nervous"
+    @@all << self
   end
   def mood
     @mood 
+  end
+  def self.all 
+    @@all 
   end 
   
 end
