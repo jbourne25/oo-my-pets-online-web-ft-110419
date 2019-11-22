ee76cac878d51e238c7f1e9cec60f996a3fa63b2
Jeremy Bourne <jbourne25@yahoo.com>
2019-11-22 22:23:31 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -72,7 +72,7 @@ class Owner
   end
   
   def list_pets 
-    puts "I have #{owner.dogs.count} dog(s), and #{owner.cats.count} cat(s)."
+    return "I have #{owner.dogs.count} dog(s), and #{owner.cats.count} cat(s)."
 
   end 
   
