e52bb4be14335a3b0d3ffb75ed1d4c837a79deda
Jeremy Bourne <jbourne25@yahoo.com>
2019-11-22 22:04:55 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -51,9 +51,7 @@ class Owner
     
   def sell_pets
     pets = self.dogs + self.cats 
-    pets.owner == self ? pets.mood = "happy" : pets.mood = "nervous" 
-  end 
-      
+    pets.owner == nil ? pets.mood = "nervous" : pets.mood = "happy" 
   end 
   
   def self.count 
